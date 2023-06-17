0f55ce18cbf864bcd9fae6552fe825542ccf9594
BIYUEHU <biyuehuya@gmail.com>
2023-06-17 11:52:08 +0800

    update list

--- plugins/claude/config.ts
+++ plugins/claude/config.ts
@@ -1,16 +1,21 @@
 export default {
     api: {
+        /* Claude接口地址 */
+        url: "https://ol7t35.laf.dev/claude-api",
         token: "xoxp-5198247082403-5198378930930-5412950741749-ebf943ccadb6bdb42b6030a857143387",
         bot: "U055UBYFQJW",
         chatId: 'ai'
     },
+    /* 群内聊天前缀 */
     groupd_prefix: "#",
     list: {
+        /* 开启的群列表 一行一个 */
         groups: [
             "673830908",
             "317691609",
             "3324656453"
         ],
+        /* 开启的QQ列表 一行一个 */
         users: []
     }
 }
