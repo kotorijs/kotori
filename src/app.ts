2005749e491eceb22d7a69af3252a30430b81169
Hotaru <biyuehuya@gmail.com>
2023-08-19 11:00:22 +0800

    chore: a few change

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@
  * @Blog: http://imlolicon.tk
  * @Date: 2023-06-24 15:12:55
  * @LastEditors: Hotaru biyuehuya@gmail.com
- * @LastEditTime: 2023-08-19 10:59:11
+ * @LastEditTime: 2023-08-19 11:00:12
  */
 import KotoriBot from '@/../main';
 
