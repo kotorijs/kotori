83bf83eba328ac55adcebf2b4387ec5277c2c385
Hotaru <biyuehuya@gmail.com>
2024-01-01 00:13:59 +0800

    feat: service, cache, module config

--- modules/goodnight/src/type.ts
+++
@@ -1,6 +0,0 @@
-import { obj } from 'kotori-bot';
-
-export default interface good {
-  morning: obj<number>;
-  night: obj<number>;
-}
--- modules/mediawiki/src/type.ts
+++
@@ -1,4 +0,0 @@
-export default interface wikiData {
-  name: string;
-  api: string;
-}
