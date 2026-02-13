0c01b241d35bdcf9c5de8f2b4e2087776b224c9b
Hotaru <biyuehuya@gmail.com>
2026-02-14 00:46:11 +0800

    styles: command help exchange with description

--- modules/tools/minecraft/src/index.ts
+++ modules/tools/minecraft/src/index.ts
@@ -70,12 +70,12 @@ const mcvSchema1 = Tsu.Object({
   )
 })
 
-const mcvSchema2 = Tsu.Array(
-  Tsu.Object({
-    name: Tsu.String(),
-    releaseDate: Tsu.String().optional()
-  })
-)
+// const mcvSchema2 = Tsu.Array(
+//   Tsu.Object({
+//     name: Tsu.String(),
+//     releaseDate: Tsu.String().optional()
+//   })
+// )
 
 export function main(ctx: Context) {
   ctx.command('motd <ip> [port:number=25565] - minecraft.descr.motd').action(async (data, session) => {
@@ -99,7 +99,7 @@ export function main(ctx: Context) {
   })
 
   ctx
-    .command('mcskin <name>')
+    .command('mcskin <name> - minecraft.descr.mcskin')
     .action(async (data, session) => {
       const res = mcskinSchema.parse(await ctx.http.get('https://api.hotaru.icu/api/mcskin', { name: data.args[0] }))
       if (!res.data) return session.format('minecraft.msg.mcskin.fail', [data.args[0]])
@@ -110,19 +110,17 @@ export function main(ctx: Context) {
         res.data.avatar ? Messages.image(`base64://${res.data.avatar.substring(22)}`) : ''
       ])
     })
-    .help('minecraft.descr.mcskin')
 
   ctx
-    .command('mcv')
+    .command('mcv - minecraft.descr.mcv')
     .action(async (_, session) => {
       const res = mcvSchema1.parse(await ctx.http.get('https://piston-meta.mojang.com/mc/game/version_manifest.json'))
-      const res2 = mcvSchema2.parse(await ctx.http.get('https://bugs.mojang.com/rest/api/2/project/10200/versions'))
+      // const res2 = mcvSchema2.parse(await ctx.http.get('https://bugs.mojang.com/rest/api/2/project/10200/versions'))
       const { release, snapshot } = res.latest
       const releaseDate = session.i18n.date(new Date(res.versions.find((el) => el.id === release)?.releaseTime ?? 0))
       const snapshotDate = session.i18n.date(new Date(res.versions.find((el) => el.id === snapshot)?.releaseTime ?? 0))
-      const { name: mcbe, releaseDate: mcbeDate } = res2.find((el) => !!el.releaseDate) ?? {}
-      const params = [release, releaseDate, snapshot, snapshotDate, mcbe, session.i18n.date(new Date(mcbeDate ?? 0))]
+      // const { name: mcbe, releaseDate: mcbeDate } = res2.find((el) => !!el.releaseDate) ?? {}
+      const params = [release, releaseDate, snapshot, snapshotDate, '', '']
       return session.format('minecraft.msg.mcv', params)
     })
-    .help('minecraft.descr.mcv')
 }
